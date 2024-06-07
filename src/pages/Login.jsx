0a8e7c186398758ede27d3f075e60b72485af000
Cristiano Gonçalves <cristianog.contato@gmail.com>
2024-06-07 20:04:17 -0300

    pagina login Jose Luiz

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react"
-import axios from "axios"
-import { Link } from 'react-router-dom';
+import { useHistory } from "react-router-dom"
+import api from "../api/api"
 
 const Login = () => {
     const [login, setLogin] = useState({email: '', senha:''})
+    const history = useHistory()
 
     const handleChange = (e) => {
         setLogin({ ...login, [e.target.name]: e.target.value });
@@ -12,27 +13,38 @@ const Login = () => {
     const handleSubmit = async (e) => {
         e.preventDefault()
         try {
-            const response = await axios.post('', login)
-            alert('Login realizado com sucesso!')
+            const response = await api.get('/users')
+           
+            const user = response.data.filter(data => data.email === login.email && data.senha === login.senha)
+
+            if (user.length > 0) {
+                alert('Login realizado com sucesso!')
+                history.push('/')
+            } else {
+                alert('Usuário ou senha inválidos')
+                handleZerar()
+            }
+
         } catch (error) {
             console.error('Erro ao realizar login', error)
         }
     }
     
     const handleZerar = () => {
-       setLogin('')
+       setLogin({
+        email: '',
+        senha: ''
+       })
     }
     return (
         <>
-            <div className="login-form">
-                <form onSubmit={handleSubmit} onReset={(e) => {
-                    handleZerar()
-                }}>
+            <div>
+                <form onSubmit={handleSubmit}>
                     <h2>Acesse o sistema</h2>
-                    <input required type="email" name="email" 
+                    <input required value={login.email} type="email" name="email" 
                     placeholder="E-mail" onChange={handleChange}/>
                     <p/>
-                    <input required type="password" name="senha" 
+                    <input required value={login.senha} type="password" name="senha" 
                     placeholder="Senha" onChange={handleChange}/>
                     <p/>
                     <label>
@@ -42,9 +54,9 @@ const Login = () => {
                     <p/>
                     <a href="#">Esqueceu a senha?</a>
                     <p/>
-                    <button onClick={handleSubmit}>Entrar</button>
-                    <br/>
-                    Não tem conta? <Link to='/Cadastro'>Registrar</Link>
+                    <button type="submit">Entrar</button>
+                    <p/>
+                    <p>Não tem conta? <a href="#">Registrar</a></p>
                 </form>
             </div>
         </>
