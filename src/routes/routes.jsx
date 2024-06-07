db5c8faacf8fab13c617dc4aefec288040a4d353
cristianog.contato@gmail.com <cristianog.contato@gmail.com>
2024-06-07 12:41:09 -0300

    correcao rota cadastro

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -4,6 +4,7 @@ import ProdutoEspecifico from '../pages/ProdutoEspecifico'
 import Carrinho from '../pages/Carrinho'
 import Categoria from '../pages/Categoria'
 import Login from '../pages/Login'
+import Cadastro from '../pages/Cadastro'
 
 const Routes = () => {
     return (
@@ -11,7 +12,7 @@ const Routes = () => {
             <BrowserRouter>
                 <Switch>
                     <Route exact path='/' component={Home} />
-                    {/* <Route exact path='/cadastro' component={Cadastro} /> */}
+                    <Route exact path='/cadastro' component={Cadastro} />
                     <Route exact path='/login' component={Login} />
                     <Route exact path='/:categoria' component={Categoria} />
                     <Route exact path='/produto/:id' component={ProdutoEspecifico} />
