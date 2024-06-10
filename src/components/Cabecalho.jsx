908d71c5eae7279e6d294b75b7ab821ec18a422c
Cristiano Gonçalves <cristianog.contato@gmail.com>
2024-06-10 16:28:00 -0300

    pagina categoria, detalhes no carrinho e logica no login

--- src/components/Cabecalho.jsx
+++ src/components/Cabecalho.jsx
@@ -7,6 +7,10 @@ const Cabecalho = () => {
                 {/* liks das categorias */}
                 <Link to='/'><h3>Ecommerce</h3></Link><br />
                 <Link to='/'>Home |</Link>
+                <ul>
+                    <li><Link to={`/categoria/eletronico`}>Eletronico</Link></li>
+                    <li><Link to={`/categoria/teclado`}>Teclado</Link></li>
+                </ul>
                 <Link to='/Login'> Login |</Link>
                 <Link to='/cadastro'> Cadastro |</Link>
                 <Link to='/carrinho'> Carrinho</Link>
