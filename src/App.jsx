f6ebdc705c20902b1e10db648f55aec769e6c640
joseluizzl <joseluizjlsb@gmail.com>
2024-06-10 21:23:17 -0300

    Adicionei o footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer/Footer";
 import Routes from "./routes/Routes";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <div className="App">
         <Routes />
+        <Footer />
       </div>
     </>
   );
