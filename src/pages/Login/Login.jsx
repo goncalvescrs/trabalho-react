c5c62efb9ce6071cf39ec623a80bd25e92eddc69
Larissa-Rahal <rahal.larissa@gmail.com>
2024-06-11 00:22:49 -0300

    mais ajustes de CSS 7

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,50 +1,50 @@
 import { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
+import { getItem, setItem } from "../../services/LocalStorageFuncs";
 import api from "../../api/api";
 import "./login.css"
 
 const Login = () => {
-  const history = useHistory();
-
-  const [login, setLogin] = useState({ email: "", senha: "" });
+  const [login, setLogin] = useState({email: '', senha:''})
+  const history = useHistory()
 
   const handleChange = (e) => {
     setLogin({ ...login, [e.target.name]: e.target.value });
   };
-
   const handleSubmit = async (e) => {
-    e.preventDefault();
-    var email = "";
-    var senha = "";
-    
-    try {
-      const { data } = await api.get("/users");
+    e.preventDefault()
+        try {
+            const response = await api.get('/users')
+            const user = response.data.filter(data => data.email === login.email && data.senha === login.senha)
 
-      for (let i = 0; i < data.length; i++) {
-        console.log(data[i].email);
-        if (login.email == data[i].email && login.senha == data[i].senha) {
-          email = data[i].email;
-          senha = data[i].senha;
+            if (user.length > 0) {
+                alert('Login realizado com sucesso!')
+                setItem('usuarioLogado', user); // Salva o usuário logado no localStorage
+
+                const carrinho = getItem('carrinho') || [];
+                if (carrinho.length === 0) {
+                    history.push('/')
+                } else {
+                    history.push('/carrinho')
+                }
+            } else {
+                alert('Usuário ou senha inválidos')
+                handleZerar()
+            }
+        } catch (error) {
+            console.error('Erro ao realizar login', error)
         }
-      }
-
-      if (email != "" && senha != "") {
-        history.push("/");
-      } else alert("Login não encontrado!");
-
-    } catch (error) {
-      console.error("Erro ao realizar login", error);
     }
-  };
-
-  const handleZerar = () => {
-    setLogin({ email: "", senha: "" });
-  };
-
+    
+    const handleZerar = () => {
+        setLogin({
+        email: '',
+        senha: ''
+    })
+    }
   const inputFocus = (title) => {
     return <span className="focus-input" data-placeholder={title}></span>;
   };
-
   return (
     <div className="container-login">
       <div className="wrap-login">
