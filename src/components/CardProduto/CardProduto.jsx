44a3b7577fb06a3dea193940103c1b372347db9f
Larissa-Rahal <rahal.larissa@gmail.com>
2024-06-10 21:25:10 -0300

    ajuste de CSS e movendo o index CardProduto para uma pasta

--- src/components/CardProduto/CardProduto.jsx
+++ src/components/CardProduto/CardProduto.jsx
@@ -7,8 +7,11 @@ const CardProduto = ({ id, nome, imgUrl, descricao, preco, likes }) => {
     return (
         <>
             <div>
+                <div>
                 <a href={`/produto/${id}`}/>
                 <img src={imgUrl} alt={descricao} />
+                </div>
+                <div>\
                 <span>
                     <h2>{nome}</h2>
                     <h4>R${preco}</h4>
@@ -16,6 +19,7 @@ const CardProduto = ({ id, nome, imgUrl, descricao, preco, likes }) => {
                     <h5>Frete Grátis <LiaShippingFastSolid/></h5>
                     <h6><CiHeart/> Curtidas ({likes})</h6>
                 </span>
+                </div>
             </div>
         </>
     )
